c8c98849937e5b5b0a9fc0e2c18ca369890710cb
Chris Bowman <stereoabusestudios@gmail.com>
2021-04-26 15:54:38 -0400

    Update anagram_detection.py

--- problems/anagram_detection.py
+++ problems/anagram_detection.py
@@ -1,6 +1,6 @@
 ##  Anagram Detection
 ##  7 kyu
-##  https://www.codewars.com//kata/529eef7a9194e0cbc1000255
+##  https://www.codewars.com/kata/529eef7a9194e0cbc1000255
 
 
 # write the function is_anagram
