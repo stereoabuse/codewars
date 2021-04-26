01154b345f995224e6741c289d3a5addba7eca94
Chris Bowman <stereoabusestudios@gmail.com>
2021-04-26 15:54:00 -0400

    Update a_needle_in_the_haystack.py

--- problems/a_needle_in_the_haystack.py
+++ problems/a_needle_in_the_haystack.py
@@ -1,6 +1,6 @@
 ##  A Needle in the Haystack
 ##  8 kyu
-##  https://www.codewars.com//kata/56676e8fabd2d1ff3000000c
+##  https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
 
 
 def find_needle(haystack):
